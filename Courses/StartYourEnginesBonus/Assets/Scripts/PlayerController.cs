b34bc35c011f5f612bb4882c8de889f8786d294a
Josh Disney <joshua.a.disney@gmail.com>
2023-06-01 22:34:07 -0700

    Complete Start Your Engines rebuild

--- Courses/StartYourEnginesBonus/Assets/Scripts/PlayerController.cs
+++ Courses/StartYourEnginesBonus/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,9 @@ public class PlayerController : MonoBehaviour
 
     // variables
     public float speed = 20.0f;
+    public float turnSpeed = 100.0f;
+    public float forwardInput;
+    public float horizontalInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +19,10 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Move the Player   
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        forwardInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+        // Move the Player forward and turn based on input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
