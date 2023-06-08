c8f97c485894ed7441d3737da15c4e68a6a461ac
Josh Disney <joshua.a.disney@gmail.com>
2023-06-07 23:59:04 -0700

    Add control functionality for first and second player

--- Courses/StartYourEnginesBonus/Assets/Scripts/Player2Controller.cs
+++ Courses/StartYourEnginesBonus/Assets/Scripts/Player2Controller.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class Player2Controller : MonoBehaviour
 {
+     // variables
     public float speed = 20.0f;
+    public float turnSpeed = 100.0f;
+    public float forwardInput;
+    public float horizontalInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +18,10 @@ public class Player2Controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        forwardInput = Input.GetAxis("VerticalP2");
+        horizontalInput = Input.GetAxis("HorizontalP2");
+        // Move the Player forward and turn based on input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
