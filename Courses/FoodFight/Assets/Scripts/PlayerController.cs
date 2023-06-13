f91c9e72176f6cffa9e2dda510614b943d130337
Josh Disney <joshua.a.disney@gmail.com>
2023-06-12 23:56:08 -0700

    Add ability to spawn and launch prefab carrot on button press

--- Courses/FoodFight/Assets/Scripts/PlayerController.cs
+++ Courses/FoodFight/Assets/Scripts/PlayerController.cs
@@ -21,10 +21,17 @@ public class PlayerController : MonoBehaviour
     {
         if (transform.position.x < -xRange) {
             transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
-        } else if (transform.position.x > xRange) {
+        }
+        
+        if (transform.position.x > xRange) {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            // Launch a projectile from the player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
     }
 }
