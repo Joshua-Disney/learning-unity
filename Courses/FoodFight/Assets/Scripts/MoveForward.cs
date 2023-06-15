6cdf579cd020e6cf5271636b08d8ec6a56a24434
Josh Disney <joshua.a.disney@gmail.com>
2023-06-14 23:20:23 -0700

    Complete Code With Unity lesson 2.2

--- Courses/FoodFight/Assets/Scripts/MoveForward.cs
+++ Courses/FoodFight/Assets/Scripts/MoveForward.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ThrowCarrot : MonoBehaviour
+public class MoveForward : MonoBehaviour
 {
-    public float speed = 40.0f;
+    public float speed = 30.0f;
     // Start is called before the first frame update
     void Start()
     {
