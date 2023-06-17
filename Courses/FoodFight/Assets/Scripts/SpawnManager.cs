4b2ad70370cc777dc5febc09cf9b875f1051899b
Josh Disney <joshua.a.disney@gmail.com>
2023-06-17 00:44:24 -0700

    Created extracted method to spawn animals, created repeating method to automate spawning

--- Courses/FoodFight/Assets/Scripts/SpawnManager.cs
+++ Courses/FoodFight/Assets/Scripts/SpawnManager.cs
@@ -8,20 +8,24 @@ public class SpawnManager : MonoBehaviour
     // Start is called before the first frame update
     private float spawnRangeX = 20.0f;
     private float spawnPosZ = 20.0f;
+    private float startDelay = 2.0f;
+    private float spawnInterval = 2.5f;
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            int animalIndex = Random.Range(0, animalPrefabs.Length);
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
 
-            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
-        }
+    }
+
+    void SpawnRandomAnimal()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
     }
 }
