504f5518706578212fd42d57bcdf8ccc95f6d083
Josh Disney <joshua.a.disney@gmail.com>
2023-06-22 00:15:52 -0700

    Almost have dog timer strech goal working

--- Courses/PlayFetch/Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Courses/PlayFetch/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
+    public float resetTimer = 0;
+    public bool dogCanRun = true;
 
     // Update is called once per frame
     void Update()
@@ -12,7 +14,16 @@ public class PlayerControllerX : MonoBehaviour
         // On spacebar press, send dog
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            resetTimer += Time.deltaTime;
+            if (dogCanRun)
+            {
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+                dogCanRun = false;
+            } else if (resetTimer > 2)
+            {
+                resetTimer = 0;
+                dogCanRun = true;
+            }
         }
     }
 }
