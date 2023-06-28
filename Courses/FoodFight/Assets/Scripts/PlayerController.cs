10b0f569573076c4220a0d393347926e7ac7bd97
Josh Disney <joshua.a.disney@gmail.com>
2023-06-27 23:56:46 -0700

    Add ability for player to move in all eight cardinal directions

--- Courses/FoodFight/Assets/Scripts/PlayerController.cs
+++ Courses/FoodFight/Assets/Scripts/PlayerController.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // variables
+    public float verticalInput;
     public float horizontalInput;
     public float speed = 15.0f;
-    public float xRange = 13.0f;
+    public float moveRange = 13.0f;
+    public float zBottomBound = 0.0f;
 
     public GameObject projectilePrefab;
     // Start is called before the first frame update
@@ -17,18 +19,25 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        if (transform.position.x < -xRange) 
-        {
-            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+    void Update() {
+        if (transform.position.x < -moveRange) {
+            transform.position = new Vector3(-moveRange, transform.position.y, transform.position.z);
         }
         
-        if (transform.position.x > xRange) 
-        {
-            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        if (transform.position.x > moveRange) {
+            transform.position = new Vector3(moveRange, transform.position.y, transform.position.z);
         }
+
+        if (transform.position.z < zBottomBound) {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zBottomBound);
+        }
+
+        if (transform.position.z > moveRange) {
+            transform.position = new Vector3(transform.position.x, transform.position.y, moveRange);
+        }
+        verticalInput = Input.GetAxis("Vertical");
         horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
 
         if (Input.GetKeyDown(KeyCode.Space)) 
