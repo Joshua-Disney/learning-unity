68e9b386662f34b976546d6359a5aefeca49e450
Josh Disney <joshua.a.disney@gmail.com>
2023-07-11 23:59:16 -0700

    Balloon controls not yet working

--- Courses/BBB/Assets/Challenge 3/Scripts/PlayerControllerX.cs
+++ Courses/BBB/Assets/Challenge 3/Scripts/PlayerControllerX.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerControllerX : MonoBehaviour
 {
-    public bool gameOver;
+    public bool gameOver = false;
 
     public float floatForce;
     private float gravityModifier = 1.5f;
@@ -33,9 +33,9 @@ public class PlayerControllerX : MonoBehaviour
     void Update()
     {
         // While space is pressed and player is low enough, float up
-        if (Input.GetKey(KeyCode.Space) && !gameOver)
+        if (Input.GetKeyDown(KeyCode.Space) && !gameOver)
         {
-            playerRb.AddForce(Vector3.up * floatForce);
+            playerRb.AddForce(Vector3.up * floatForce, ForceMode.Impulse);
         }
     }
 
