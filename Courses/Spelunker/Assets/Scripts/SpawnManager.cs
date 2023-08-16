56ababe75e7ab8d80c1c06621633c24252a41dc7
Josh Disney <joshua.a.disney@gmail.com>
2023-08-15 23:25:34 -0700

    Create SpawnManager game object and script

---
+++ Courses/Spelunker/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public GameObject treasurePrefab;
+    private float spawnRange = 20.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
