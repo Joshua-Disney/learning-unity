403920b0ecd05d7b2c8044ed1d7bf72e0da62b44
Josh Disney <joshua.a.disney@gmail.com>
2023-08-16 23:37:26 -0700

    Attempt 1 at fixing broken treasureCount failed

--- Courses/Spelunker/Assets/Scripts/SpawnManager.cs
+++ Courses/Spelunker/Assets/Scripts/SpawnManager.cs
@@ -8,23 +8,21 @@ public class SpawnManager : MonoBehaviour
     public GameObject treasurePrefab;
     private int treasureCount;
     private float spawnRange = 20.0f;
+    private int numOfTreasures = 3;
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < 3; i++) {
-            Instantiate(enemyPrefab, GenerateSpawnPos(), enemyPrefab.transform.rotation);
-            Instantiate(treasurePrefab, GenerateSpawnPos(), treasurePrefab.transform.rotation);
-        }
-        
+        SpawnTreasures(numOfTreasures);
+        Instantiate(enemyPrefab, GenerateSpawnPos(), enemyPrefab.transform.rotation);
     }
 
     // Update is called once per frame
     void Update()
     {
-        // treasureCount = FindObjectsOfType<Treasure>().Length;
-        // if (treasureCount == 0) {
-        //     Debug.Log("YOU FOUND ALL THE TREASURE!!!");
-        // }
+        treasureCount = FindObjectsOfType<Treasure>().Length || numOfTreasures;
+        if (treasureCount == 0) {
+            Debug.Log("YOU FOUND ALL THE TREASURE!!!");
+        }
     }
 
     private Vector3 GenerateSpawnPos() {
@@ -35,5 +33,11 @@ public class SpawnManager : MonoBehaviour
         Vector3 spawnPos = new Vector3(spawnPosX, spawnPosY, spawnPosZ);
 
         return spawnPos;
+    }
+
+    void SpawnTreasures(int treasuresToSpawn) {
+        for (int i = 0; i < treasuresToSpawn; i++) {
+            Instantiate(treasurePrefab, GenerateSpawnPos(), treasurePrefab.transform.rotation);
+        }
     }
 }
