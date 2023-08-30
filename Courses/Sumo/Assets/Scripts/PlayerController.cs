2fd9cdaf6c22e72c9183e2f1d0c66e5b516626b1
Josh Disney <joshua.a.disney@gmail.com>
2023-08-30 00:25:40 -0700

    Build all logic for multiple powerups and for Missile Behavior

--- Courses/Sumo/Assets/Scripts/PlayerController.cs
+++ Courses/Sumo/Assets/Scripts/PlayerController.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Private Variables
+    // Variables
     private Rigidbody playerRb;
     private GameObject focalPoint;
     private float powerUpForce = 15.0f;
-    // Public Variables
     public float speed = 2.0f;
     public bool isPoweredUp = false;
     public GameObject powerupIndicator;
+    public PowerUpType currentPowerUp = PowerUpType.None;
+    public GameObject missilePrefab;
+    private GameObject tmpMissile;
+    private Coroutine powerUpCountdown;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,21 +27,46 @@ public class PlayerController : MonoBehaviour
     {
         float forwardInput = Input.GetAxis("Vertical");
         playerRb.AddForce(focalPoint.transform.forward * forwardInput * speed);
+        powerupIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
+        if (currentPowerUp == PowerUpType.Missile && Input.GetKeyDown(KeyCode.F)) {
+            LaunchMissiles();
+        }
     }
 
     private void OnTriggerEnter(Collider other) {
         if (other.CompareTag("Powerup")) {
             isPoweredUp = true;
+            currentPowerUp = other.gameObject.GetComponent<PowerUp>().powerUpType;
             powerupIndicator.gameObject.SetActive(true);
             Destroy(other.gameObject);
+
+            if (powerUpCountdown != null) {
+                StopCoroutine(powerUpCountdown);
+            }
+            powerUpCountdown = StartCoroutine(powerUpCountdownRoutine());
         }
     }
 
+    IEnumerator powerUpCountdownRoutine() {
+        yield return new WaitForSeconds(7);
+        isPoweredUp = false;
+        currentPowerUp = PowerUpType.None;
+        powerupIndicator.gameObject.SetActive(false);
+    }
+
     private void OnCollisionEnter(Collision collision) {
-        if (collision.gameObject.CompareTag("Enemy")) {
+        if (collision.gameObject.CompareTag("Enemy") && currentPowerUp == PowerUpType.Repel) {
             Rigidbody enemyRb = collision.gameObject.GetComponent<Rigidbody>();
             Vector3 awayFromPlayer = collision.gameObject.transform.position - transform.position;
             enemyRb.AddForce(awayFromPlayer * powerUpForce, ForceMode.Impulse);
+            Debug.Log("Player collided with: " + collision.gameObject.name + " with powerup set to " + currentPowerUp.ToString());
+        }
+    }
+
+    void LaunchMissiles () {
+        foreach (var enemy in FindObjectsOfType<Enemy>()) {
+            tmpMissile = Instantiate(missilePrefab, transform.position + Vector3.up, Quaternion.identity);
+            tmpMissile.GetComponent<MissileBehavior>().Fire(enemy.transform);
         }
     }
 }
--- Courses/Sumo/Assets/Scripts/SpawnManager.cs
+++ Courses/Sumo/Assets/Scripts/SpawnManager.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] enemyPrefabs;
-    public GameObject powerupPrefab;
+    public GameObject[] powerupPrefabs;
     public int enemyCount;
     public int waveNumber = 1;
     private float spawnRange = 9.0f;
     // Start is called before the first frame update
     void Start()
     {
+        int randomPowerup = Random.Range(0, powerupPrefabs.Length);
+        Instantiate(powerupPrefabs[randomPowerup], GenerateSpawnPos(), powerupPrefabs[randomPowerup].transform.rotation);
         SpawnEnemyWave(waveNumber);
     }
 
@@ -22,6 +24,8 @@ public class SpawnManager : MonoBehaviour
         if (enemyCount == 0) {
             waveNumber++;
             SpawnEnemyWave(waveNumber);
+            int randomPowerup = Random.Range(0, powerupPrefabs.Length);
+            Instantiate(powerupPrefabs[randomPowerup], GenerateSpawnPos(), powerupPrefabs[randomPowerup].transform.rotation);
         }
     }
 
