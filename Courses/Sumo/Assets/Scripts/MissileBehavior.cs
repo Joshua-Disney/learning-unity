659d0724d5fc45d2fefd518339a256ea12fb3168
Josh Disney <joshua.a.disney@gmail.com>
2023-08-29 23:55:51 -0700

    Significant progress made.  I think I found where I messed up last time

---
+++ Courses/Sumo/Assets/Scripts/MissileBehavior.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissileBehavior : MonoBehaviour
+{
+    // Variables
+    private Transform target;
+    private float speed = 15.0f;
+    private bool homing;
+    private float missileStrength = 15.0f;
+    private float aliveTimer = 5.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (homing && target != null) {
+            Vector3 moveDirection = (target.transform.position - transform.position).normalized;
+            transform.position += moveDirection * speed * Time.deltaTime;
+            transform.LookAt(target);
+        }
+    }
+
+    public void Fire(Transform homingTarget)
+    {
+        target = homingTarget;
+        homing = true;
+        Destroy(gameObject, aliveTimer);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag(target.tag)) {
+            Rigidbody targetRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 away = -collision.contacts[0].normal;
+            targetRigidbody.AddForce(away * missileStrength, ForceMode.Impulse);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Courses/Sumo/Assets/Scripts/PowerUp.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PowerUpType { None, Repel, Missiles}
+
+public class PowerUp : MonoBehaviour
+{
+    public PowerUpType powerUpType;
+}
