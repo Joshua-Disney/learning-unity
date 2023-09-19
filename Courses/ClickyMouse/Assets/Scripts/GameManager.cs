7793d8715fcde52307949077ec544bb3e5c0117d
Josh Disney <joshua.a.disney@gmail.com>
2023-09-18 23:46:54 -0700

    Set score text variable

--- Courses/ClickyMouse/Assets/Scripts/GameManager.cs
+++ Courses/ClickyMouse/Assets/Scripts/GameManager.cs
@@ -1,15 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     private float spawnRate = 1.0f;
+    private int score;
     public List<GameObject> targets;
+    public TextMeshProUGUI scoreText;
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(spawnTarget());
+        score = 0;
+        scoreText.text = "Score: " + score;
     }
 
     // Update is called once per frame
